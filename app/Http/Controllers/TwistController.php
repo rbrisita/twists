14e7b3d88d248bfed2361cec6023bf4ddb8b89ee
Robert Brisita <rbrisita@hotmail.com>
2019-06-06 17:21:51 -0400

    Added caching response wrapped Redis data.

--- app/Http/Controllers/TwistController.php
+++ app/Http/Controllers/TwistController.php
@@ -3,37 +3,66 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Redis;
 use Symfony\Component\HttpFoundation\Response as HttpResponse;
 
 class TwistController extends Controller
 {
+    private $last_id;
+    private $secs_in_day;
+
+    public function __construct()
+    {
+        $this->secs_in_day = 86400;
+        $this->last_id = Cache::remember('last_id', $this->secs_in_day, function () {
+            $data = Redis::get('topics');
+            $arr = json_decode($data);
+            $last_obj = last($arr);
+            return $last_obj->id;
+        });
+    }
+
     public function topics()
     {
-        return response()->json([
-            'data' => Redis::get('topics')
-        ]);
+        return Cache::remember('topics', $this->secs_in_day, function () {
+            return response()->json([
+                'data' => Redis::get('topics')
+            ]);
+        });
     }
 
     public function topicById(int $id)
     {
-        $response = null;
-        $http_code = HttpResponse::HTTP_FOUND;
-
-        $data = Redis::get($id);
-        if (!$data) {
-            $http_code = HttpResponse::HTTP_NOT_FOUND;
+        if ($id < 0 || $id > $this->last_id) {
+            $http_code = HttpResponse::HTTP_BAD_REQUEST;
             $response = [
                 'code' => $http_code,
                 'error' => HttpResponse::$statusTexts[$http_code],
-                'status' => 'Topic not found'
+                'status' => 'Id not valid'
             ];
-        } else {
-            $response = [
-                'data' => $data
-            ];
+            return response()->json($response, $http_code);
         }
 
-        return response()->json($response, $http_code);
+        return Cache::remember('topic_' . $id, $this->secs_in_day, function () use ($id) {
+            $response = null;
+            $http_code = HttpResponse::HTTP_FOUND;
+
+            $data = Redis::get($id);
+            if (!$data) {
+                $http_code = HttpResponse::HTTP_NOT_FOUND;
+                $response = [
+                    'code' => $http_code,
+                    'error' => HttpResponse::$statusTexts[$http_code],
+                    'status' => 'Topic not found'
+                ];
+            } else {
+                $response = [
+                    'data' => $data
+                ];
+            }
+
+            return response()->json($response, $http_code);
+        });
     }
 }
