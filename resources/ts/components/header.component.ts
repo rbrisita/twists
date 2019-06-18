031dabd42cad744ab02ec20a5f01c0db7acb6107
Robert Brisita <rbrisita@hotmail.com>
2019-06-18 19:15:15 -0400

    Added styles and classes to fix Safari issues and have a better fixed header bar.

--- resources/ts/components/header.component.ts
+++ resources/ts/components/header.component.ts
@@ -1,18 +1,23 @@
 import {
     Component,
     ElementRef,
+    OnDestroy,
     OnInit
 } from '@angular/core';
+
+import '../../../node_modules/foundation-sites/dist/js/foundation';
 
 @Component({
     selector: 'app-header',
     templateUrl: './views/header.component.html',
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnDestroy, OnInit {
 
     constructor(private element: ElementRef) { }
 
     ngOnInit() {
+        this.handleHeaderWidth();
+
         setImmediate(() => {
             // Remove tabindex to eliminate focus on header menu options.
             const arr: Element[] = this.element.nativeElement.querySelectorAll('li > a');
@@ -20,5 +25,45 @@ export class HeaderComponent implements OnInit {
                 item.removeAttribute('tabindex');
             });
         });
+
+        this.listenToResize();
+    }
+
+    ngOnDestroy() {
+        window.removeEventListener('resize', this.handleHeaderWidth);
+    }
+
+    private listenToResize(): void {
+        window.addEventListener('resize', this.handleHeaderWidth);
+    }
+
+    /**
+     * Adjust max width of header to keep a consistent look.
+     */
+    private handleHeaderWidth(): void {
+        const container: HTMLElement | null = document.querySelector('.sticky-container');
+        if (!container) {
+            console.error('No container');
+            return;
+        }
+
+        const width: number = container.getBoundingClientRect().width;
+        const comp: CSSStyleDeclaration = window.getComputedStyle(container);
+        const pdngl: number = parseInt(comp['padding-left'], 10);
+        const pdngr: number = parseInt(comp['padding-right'], 10);
+
+        const header: HTMLElement | null = document.querySelector('header');
+        if (!header) {
+            console.error('No header');
+            return;
+        }
+
+        let max_width: number = (width - pdngl - pdngr);
+        let unit: string = 'px';
+        if (Foundation.MediaQuery.current === 'small') {
+            max_width = 100;
+            unit = '%';
+        }
+        header.setAttribute('style', 'max-width:' + max_width + unit);
     }
 }
