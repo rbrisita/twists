829d4f162a9ae6ece5ffcc4df81734bf09581c69
Robert Brisita <rbrisita@hotmail.com>
2019-07-03 20:25:46 -0400

    Adding browser ico, home screen icons, iOS PWA splash screens, and radius corners for overlays.

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -23,6 +23,65 @@
         });
         gtag('config', 'UA-142688054-1');
     </script>
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <link rel="apple-touch-icon" sizes="57x57" href="/imgs/ico/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/imgs/ico/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/imgs/ico/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/imgs/ico/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/imgs/ico/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/imgs/ico/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/imgs/ico/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/imgs/ico/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/imgs/ico/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="/imgs/ico/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/imgs/ico/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/imgs/ico/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/imgs/ico/favicon-16x16.png">
+    <link rel="manifest" href="/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/imgs/ico/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+
+    {{-- iPhone 5, SE (640px x 1136px) --}}
+    <link href="/imgs/splash/iphone5_splash.png"
+        media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
+        rel="apple-touch-startup-image" />
+    {{-- iPhone 8, 7, 6s, 6 (750px x 1334px) --}}
+    <link href="/imgs/splash/iphone6_splash.png"
+        media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
+        rel="apple-touch-startup-image" />
+    {{-- iPhone 8 Plus, 7 Plus, 6s Plus, 6 Plus (1242px x 2208px) --}}
+    <link href="/imgs/splash/iphoneplus_splash.png"
+        media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)"
+        rel="apple-touch-startup-image">
+    {{-- iPhone X, Xs (1125px x 2436px) --}}
+    <link href="/imgs/splash/iphonex_splash.png"
+        media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
+        rel="apple-touch-startup-image" />
+    {{-- iPhone Xr (828px x 1792px) --}}
+    <link href="/imgs/splash/iphonexr_splash.png"
+        media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
+        rel="apple-touch-startup-image" />
+    {{-- iPhone Xs Max (1242px x 2688px) --}}
+    <link href="/imgs/splash/iphonexsmax_splash.png"
+        media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
+        rel="apple-touch-startup-image" />
+    {{-- iPad Mini, Air (1536px x 2048px) --}}
+    <link href="/imgs/splash/ipad_splash.png"
+        media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)"
+        rel="apple-touch-startup-image" />
+    {{-- iPad Pro 10.5" (1668px x 2224px) --}}
+    <link href="/imgs/splash/ipadpro1_splash.png"
+        media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)"
+        rel="apple-touch-startup-image" />
+    {{-- iPad Pro 11” (1668px x 2388px) --}}
+    <link href="/imgs/splash/ipadpro3_splash.png"
+        media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)"
+        rel="apple-touch-startup-image" />
+    {{-- iPad Pro 12.9" (2048px x 2732px) --}}
+    <link href="/imgs/splash/ipadpro2_splash.png"
+        media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)"
+        rel="apple-touch-startup-image" />
 </head>
 
 <body>
